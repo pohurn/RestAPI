65e7f7ae1ec338fe009dfcf8f9f5c85444123887
Navindranath Pohur <pohur95@gmail.com>
2022-01-07 23:24:27 +0400

    intial commit

---
+++ RestApi/Controllers/EmployeeController.cs
@@ -0,0 +1,56 @@
+﻿using RestApi.Models;
+using System;
+using System.Web.Http;
+
+namespace RestApi.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        [HttpPost]
+        public bool AddEmpDetails()
+        {
+            return true;
+            //write insert logic  
+
+        }
+        [HttpGet]
+        public EmployeeModel[] Get()
+        {
+            return new EmployeeModel[]
+            {
+                new EmployeeModel
+                {
+                    id = 1,
+                    firstname = "sam",
+                    lastname = "employeeONE",
+                    address = "Port louis",
+                    age = 23
+                },
+                new EmployeeModel
+                {
+                    id = 2,
+                    firstname = "John",
+                    lastname = "employeeTWO",
+                    address = "Pamplemousses",
+                    age = 30
+                },
+                new EmployeeModel
+                {
+                    id = 3,
+                    firstname = "Alex",
+                    lastname = "employeeTHREE",
+                    address = "Terre Rouge",
+                    age = 42
+                }
+            };
+        }
+
+
+        [HttpDelete]
+        public string DeleteEmpDetails(string id)
+        {
+            return "Employee details deleted having Id " + id;
+
+        }
+    }
+}
---
+++ RestApi/Models/EmployeeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestApi.Models
+{
+    public class EmployeeModel
+    {
+        public int id { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public int age { get; set; }
+        public string address { get; set; }
+    }
+}
